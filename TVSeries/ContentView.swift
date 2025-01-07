7f8032bf3b6876c6d24a57c86d3ed80d46adb7dc
Matheus Campos <matheusbc@gmail.com>
2025-01-07 12:25:55 -0300

    Initial Commit

---
+++ TVSeries/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TVSeries
+//
+//  Created by Matheus Campos on 07/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TVSeries/TVSeriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVSeriesApp.swift
+//  TVSeries
+//
+//  Created by Matheus Campos on 07/01/25.
+//
+
+import SwiftUI
+
+@main
+struct TVSeriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
